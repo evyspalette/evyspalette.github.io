56db34731d86b91c47ec46d266006ba5e0a03271
evyspalette <evy.jan7@gmail.com>
2017-06-08 22:45:54 +0530

    Adding files

---
+++ Script_Portfolio.js
@@ -0,0 +1,25 @@
+
+function flip(cardID){
+	var cardFront="front"+cardID;
+	var cardBack="back"+cardID;
+	document.getElementById(cardFront).classList.toggle("hide");
+	document.getElementById(cardBack).classList.toggle("show");
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("pictureSlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
